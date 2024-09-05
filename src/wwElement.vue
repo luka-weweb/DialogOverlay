abf9b6136c27fcc10853cf76d2c6dc198aaa1848
luka-weweb <luka@weweb.io>
2024-09-05 12:02:21 +0200

    fix

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,57 +1,9 @@
 <template>
-  <Teleport
-    :to="teleportTarget"
-    v-if="isTeleportReady"
-    :disabled="this.content.teleport"
-  >
-    <wwLayout v-bind="$attrs" path="slot" />
-  </Teleport>
+  <div style="width: 100px; height: 100px; background-color: red"></div>
 </template>
 
 <script>
-import { Teleport } from "vue";
-
-export default {
-  inheritAttrs: false,
-  inject: ["activeTabProvided"],
-  props: {
-    content: { type: Object, required: true },
-    wwFrontState: { type: Object, required: true },
-    wwEditorState: { type: Object, required: true },
-  },
-  data() {
-    return {
-      teleportTarget: null,
-      isTeleportReady: false,
-    };
-  },
-  mounted() {
-    this.$nextTick(() => {
-      this.setupTeleport();
-    });
-  },
-  methods: {
-    setupTeleport() {
-      const frontDocument = wwLib.getFrontDocument();
-      if (frontDocument) {
-        const appEl = frontDocument.querySelector("#app");
-        if (appEl) {
-          this.teleportTarget = appEl;
-          this.isTeleportReady = true;
-        } else {
-          console.error("Target element '#app' not found in front document");
-        }
-      } else {
-        console.error("Unable to get front document");
-      }
-    },
-  },
-  watch: {
-    "content.teleport"(newValue) {
-      this.setupTeleport();
-    },
-  },
-};
+export default {};
 </script>
 
 <style scss></style>
